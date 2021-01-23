b135d1cec16ac266839b09f270b74839424c03da
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2021-01-22 20:08:23 -0800

    added passage output

--- TwoCities.cpp
+++ TwoCities.cpp
@@ -1,6 +1,17 @@
+//TwoCities.cpp
+//Created by Henryk Sosnowski 01/22/2021
+//Prints an excerpt from A Tale of Two Cities
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "A Tale of Two Cities\nBy Charles Dickens\n" << std::endl;
+    std::cout << "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of"
+                 " foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light,"
+                 " it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had "
+                 "everything before us, we had nothing before us, we were all going direct to Heaven, we were all going"
+                 " direct the other way—in short, the period was so far like the present period, that some of its"
+                 " noisiest authorities insisted on its being received, for good or for evil, in the superlative degree "
+                 "of comparison only.\n";
     return 0;
 }
